cdfbe8d98f0fc0a12931742a6bac51ea2b5472cd
Hao_Guanjun <haoguanjun@gmail.com>
2021-06-08 22:43:20 +0800

    make it work at net5.0 and net6.0

--- 07 - Testing MVC Applications/WorkingWithVisualStudio/WorkingWithVisualStudio/Startup.cs
+++ 07 - Testing MVC Applications/WorkingWithVisualStudio/WorkingWithVisualStudio/Startup.cs
@@ -6,18 +6,20 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace WorkingWithVisualStudio {
     public class Startup {
 
 
         public void ConfigureServices(IServiceCollection services) {
-            services.AddMvc();
+            services.AddMvc( options =>
+                options.EnableEndpointRouting = false
+            );
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
             app.UseDeveloperExceptionPage();
-            app.UseBrowserLink();
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
         }
