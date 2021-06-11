40e8f985fab678ba8b8717330c991790cf80e9dd
Hao_Guanjun <haoguanjun@gmail.com>
2021-06-12 06:40:08 +0800

    make it work at net5.0 and net6.0

--- 17 - Controllers and Actions/ControllersAndActions/ControllersAndActions/Startup.cs
+++ 17 - Controllers and Actions/ControllersAndActions/ControllersAndActions/Startup.cs
@@ -6,21 +6,27 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace ControllersAndActions {
     public class Startup {
 
         public void ConfigureServices(IServiceCollection services) {
-            services.AddMvc();
+            services.AddMvc( options =>
+                options.EnableEndpointRouting = false
+            );
             services.AddMemoryCache();
             services.AddSession();
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
             app.UseStatusCodePages();
             app.UseDeveloperExceptionPage();
             app.UseStaticFiles();
             app.UseSession();
+
+            // https://stackoverflow.com/questions/57684093/using-usemvc-to-configure-mvc-is-not-supported-while-using-endpoint-routing
+            // app.EnableEndpointRouting();
             app.UseMvcWithDefaultRoute();
         }
     }
