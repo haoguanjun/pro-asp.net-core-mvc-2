aff5a30fe70127bdbae8a3d8e7f8fa4a56a21263
Hao_Guanjun <haoguanjun@gmail.com>
2021-06-12 07:14:35 +0800

    make it work at net5.0 and net6.0

--- 22 - View Components/UsingViewComponents/UsingViewComponents/Startup.cs
+++ 22 - View Components/UsingViewComponents/UsingViewComponents/Startup.cs
@@ -15,10 +15,12 @@ namespace UsingViewComponents {
         public void ConfigureServices(IServiceCollection services) {
             services.AddSingleton<IProductRepository, MemoryProductRepository>();
             services.AddSingleton<ICityRepository, MemoryCityRepository>();
-            services.AddMvc();
+            services.AddMvc( options =>
+                options.EnableEndpointRouting = false
+            );
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
             app.UseStatusCodePages();
             app.UseDeveloperExceptionPage();
             app.UseStaticFiles();
