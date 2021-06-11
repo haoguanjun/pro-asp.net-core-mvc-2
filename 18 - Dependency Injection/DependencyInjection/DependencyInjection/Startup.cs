b8f77a1ce7af1c598d2bb32e19a096514bfdf95f
Hao_Guanjun <haoguanjun@gmail.com>
2021-06-12 06:51:42 +0800

    make it work at net5.0 and net6.0

--- 18 - Dependency Injection/DependencyInjection/DependencyInjection/Startup.cs
+++ 18 - Dependency Injection/DependencyInjection/DependencyInjection/Startup.cs
@@ -6,23 +6,26 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using DependencyInjection.Infrastructure;
 using DependencyInjection.Models;
 
 namespace DependencyInjection {
     public class Startup {
-        private IHostingEnvironment env;
+        private IWebHostEnvironment env;
 
-        public Startup(IHostingEnvironment hostEnv) => env = hostEnv;
+        public Startup(IWebHostEnvironment hostEnv) => env = hostEnv;
 
         public void ConfigureServices(IServiceCollection services) {
             services.AddSingleton<IRepository, MemoryRepository>();
             services.AddTransient<IModelStorage, DictionaryStorage>();
             services.AddTransient<ProductTotalizer>();
-            services.AddMvc();
+            services.AddMvc( options =>
+                options.EnableEndpointRouting = false
+            );
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
             app.UseStatusCodePages();
             app.UseDeveloperExceptionPage();
             app.UseStaticFiles();
