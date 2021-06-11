bc8471b8146bcb80acf5cd9daf93e1bf5c773dbd
Hao_Guanjun <haoguanjun@gmail.com>
2021-06-12 07:33:39 +0800

    make it work at net5.0 and net6.0

--- 29 - Applying Identity/Users/Users/Startup.cs
+++ 29 - Applying Identity/Users/Users/Startup.cs
@@ -6,48 +6,55 @@ using Microsoft.EntityFrameworkCore;
 using Users.Models;
 using Users.Infrastructure;
 
-namespace Users {
+namespace Users
+{
 
-    public class Startup {
+    public class Startup
+    {
 
         public Startup(IConfiguration configuration) =>
             Configuration = configuration;
 
         public IConfiguration Configuration { get; }
 
-        public void ConfigureServices(IServiceCollection services) {
+        public void ConfigureServices(IServiceCollection services)
+        {
 
             services.AddTransient<IPasswordValidator<AppUser>,
                 CustomPasswordValidator>();
-            services.AddTransient<IUserValidator<AppUser>, 
+            services.AddTransient<IUserValidator<AppUser>,
                 CustomUserValidator>();
 
             services.AddDbContext<AppIdentityDbContext>(options =>
                 options.UseSqlServer(
                     Configuration["Data:SportStoreIdentity:ConnectionString"]));
 
-services.AddIdentity<AppUser, IdentityRole>(opts => {
-    opts.User.RequireUniqueEmail = true;
-    //opts.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyz";
-    opts.Password.RequiredLength = 6;
-    opts.Password.RequireNonAlphanumeric = false;
-    opts.Password.RequireLowercase = false;
-    opts.Password.RequireUppercase = false;
-    opts.Password.RequireDigit = false;
-}).AddEntityFrameworkStores<AppIdentityDbContext>()
-    .AddDefaultTokenProviders();
+            services.AddIdentity<AppUser, IdentityRole>(opts =>
+            {
+                opts.User.RequireUniqueEmail = true;
+                //opts.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyz";
+                opts.Password.RequiredLength = 6;
+                opts.Password.RequireNonAlphanumeric = false;
+                opts.Password.RequireLowercase = false;
+                opts.Password.RequireUppercase = false;
+                opts.Password.RequireDigit = false;
+            }).AddEntityFrameworkStores<AppIdentityDbContext>()
+                .AddDefaultTokenProviders();
 
-            services.AddMvc();
+            services.AddMvc(options =>
+               options.EnableEndpointRouting = false
+            );
         }
 
-public void Configure(IApplicationBuilder app) {
-    app.UseStatusCodePages();
-    app.UseDeveloperExceptionPage();
-    app.UseStaticFiles();
-    app.UseAuthentication();
-    app.UseMvcWithDefaultRoute();
-    AppIdentityDbContext.CreateAdminAccount(app.ApplicationServices,
-        Configuration).Wait();
-}
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseStatusCodePages();
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseMvcWithDefaultRoute();
+            AppIdentityDbContext.CreateAdminAccount(app.ApplicationServices,
+                Configuration).Wait();
+        }
     }
 }
